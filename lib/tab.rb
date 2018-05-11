89fdf16a2deb44af531a132d59c9750178d838a4
Thomas F <taforres@asu.edu>
2018-05-11 10:19:25 -0700

    Add tab db operations and tests

---
+++ lib/tab.rb
@@ -0,0 +1,51 @@
+require './models/models'
+
+# BeerTab contains the logic to modify the tabs of beer
+class BeerTab
+  class << self
+    def add_to_tab(owes, owed)
+      ensure_user(owes)
+      ensure_user(owed)
+
+      tab = ensure_tab(owes.id, owed.id)
+      tab.count += 1
+      tab.save
+    end
+
+    def pay_tab(owes, owed)
+      ensure_user(owes)
+      ensure_user(owed)
+
+      tab = ensure_tab(owes.id, owed.id)
+
+      if tab.count.zero?
+        false
+      end
+      tab.count -= 1
+      tab.save
+      true
+    end
+
+    def ensure_tab(owes_id, owed_id)
+        Tab.find_or_create_by!(count: 0,
+                               owes: owes_id,
+                               owed: owed_id)
+    end
+
+    def show_owed(owed_id)
+      Tab.find_by(owed: owed_id).sum(:count)
+    end
+
+    def show_owes(owes_id)
+      Tab.find_by(owes: owes_id).sum(:count)
+    end
+
+    def ensure_user(user)
+      User.find_or_create_by!(telegram_id: user.id,
+                              first_name: user.first_name,
+                              last_name: user.last_name,
+                              username: user.username)
+
+    end
+  end
+end
---
+++ lib/test_tab.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require 'active_record'
+
+require './lib/tab'
+require './models/models'
+
+# TestTab ensure the database operations are correct
+class TestTab < Minitest::Test
+
+  MockUser = Struct.new(
+      :id,
+      :first_name,
+      :last_name,
+      :username
+  )
+  # Create in memory database for testing
+  # NEVER use anything else
+  # These 3 functions should only run once
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+  create_user_table
+  create_tab_table
+  def after
+    Tab.delete_all
+    User.delete_all
+  end
+
+  def test_ensure_user
+    owes = MockUser.new
+    owes.id = 10
+    owes.first_name = 'bob'
+    owes.last_name = 'meme'
+    owes.username = 'memebob'
+
+    BeerTab.ensure_user(owes)
+    u = User.find_by telegram_id: 10
+    assert_equal(10, u.telegram_id)
+  end
+
+  def test_ensure_tab
+    owes_id = 0
+    owed_id = 1
+    BeerTab.ensure_tab(owes_id, owed_id)
+    t = Tab.where(['owes = ?  and owed = ?', owes_id, owed_id]).take
+    assert_equal(t.owes, owes_id)
+    assert_equal(t.owed, owed_id)
+    assert_equal(0, t.count)
+  end
+end
